fb8f97a10f99b455dfc3d4f87c80795b9b1800c3
Lenovo <kutakov03@mail.ru>
2023-04-08 19:03:36 +0300

    task06

--- StudentJavaRevision03TaskProject-master/src/by/itstep/javatraining/revision/task/Task06.java
+++ StudentJavaRevision03TaskProject-master/src/by/itstep/javatraining/revision/task/Task06.java
@@ -24,6 +24,10 @@ package by.itstep.javatraining.revision.task;
 
 public class Task06 {
     public static boolean task06(int x1, int y1, int x2, int y2) {
-        return false;
+
+        return x1 > 0 && x2 > 0 && y1 > 0 && y2 > 0 && x1 < 9 && x2 < 9 && y1 < 9 && y2 < 9 &&
+                (Math.abs(x2 - x1) == 2 && Math.abs(y2 - y1) == 1) ||
+                (Math.abs(x2 - x1) == 1 && Math.abs(y2 - y1) == 2);
+
     }
 }
